2424ec8eeb9f335232e36ad629ded18acc54fbd2
Nomomingi <makolelashibambu50@gmail.com>
2025-09-24 23:24:11 +0200

    Seeing if my changes do something

--- GuestBookingApplication/Form1.Designer.cs
+++ GuestBookingApplication/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.Location = new Point(197, 112);
+            button1.Name = "button1";
+            button1.Size = new Size(449, 223);
+            button1.TabIndex = 0;
+            button1.Text = "67";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button button1;
     }
 }
--- GuestBookingApplication/Form1.cs
+++ GuestBookingApplication/Form1.cs
@@ -6,5 +6,10 @@ namespace GuestBookingApplication
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
